e4f86b0469d10d4ba4ec4b1f3b37c2ed0da38978
kanin <88888222+maxkanin@users.noreply.github.com>
2024-05-22 20:12:02 +0700

    add input value

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppService } from './app.service';
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const someService = app.get(AppService);
-  const ans = someService.merge([], [], []);
+  const collection_1 = [1, 2, 3];
+  const collection_2 = [13, 12, 11];
+  const collection_3 = [1, 2, 3];
+  const ans = someService.merge(collection_1, collection_2, collection_3);
   console.log(ans);
   await app.close();
 }
