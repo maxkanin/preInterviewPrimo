641771c33d5f6c62f30379a1f3f34eaab6b0396c
kanin <88888222+maxkanin@users.noreply.github.com>
2024-05-22 18:00:02 +0700

    add unitTest

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -15,4 +15,18 @@ describe('AppService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  it('should return empty array when all of the inputs are empty', () => {
+    const expected = [];
+    const res = service.merge([], [], []);
+    expect(res).toEqual(expected);
+  });
+  it('should return sorted array in ascending order', () => {
+    const expected = [1, 1, 2, 4, 5, 10, 10, 20, 100, 200, 1000, 1001];
+    const res = service.merge(
+      [1, 2, 4, 10],
+      [1001, 1000, 20, 10, 1],
+      [5, 100, 200],
+    );
+    expect(res).toEqual(expected);
+  });
 });
